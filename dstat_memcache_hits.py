7c528b34ea2d703de67e22d170a96ac7aec8d1b7
Dean Wilson <dean.wilson@gmail.com>
2009-06-30 00:20:56 +0100

    Added simple memcached plugin for dstat

---
+++ dstat_memcache_hits.py
@@ -0,0 +1,28 @@
+### Dstat Display Memcache Hit Count plugin
+### Displays the number of Memcache "get_hits" and "get_misses"
+###
+### Authority: dean.wilson@gmail.com
+
+class dstat_memcache_hits(dstat):
+    def __init__(self):
+        self.name   = 'Memcache Hits'
+        self.format = ('d', 6, 50)
+        self.nick   = ('Hit', 'Miss')
+        self.vars   = ('get_hits', 'get_misses')
+
+        self.init(self.vars, 1)
+
+    def check(self):
+        try:
+            global memcache
+            import memcache
+        except:
+            raise Exception, 'Module needs the memcache module.'
+        return True
+
+    def extract(self):
+        mc = memcache.Client(['127.0.0.1:11211'], debug=0)
+        stats = mc.get_stats()
+
+        self.val['get_hits']   = int( stats[0][1]['get_hits'] )
+        self.val['get_misses'] = int( stats[0][1]['get_misses'] )
